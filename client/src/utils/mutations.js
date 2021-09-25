973869d5409f4d36106c91b646b6d148f844f3b9
Ana Ea <aabad0464@gmail.com>
2021-09-25 02:20:01 -0600

    gql for login

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,24 @@
+import gql from 'graphql-tag';
+
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+        token
+            user {
+                _id
+                username
+                email
+                bookCount
+                savedBooks {
+                    bookId
+                    title
+                    description
+                    authors
+                    link
+                    image
+         }
+       }
+     }
+   }
+`;
+

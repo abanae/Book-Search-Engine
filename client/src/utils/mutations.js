98799cdb54739d983409d6af1af90fb909100ff7
Ana Ea <aabad0464@gmail.com>
2021-09-25 02:21:11 -0600

    gql for adding user

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -21,4 +21,24 @@ export const LOGIN_USER = gql`
      }
    }
 `;
-
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+        token
+            user {
+                _id
+                username
+                email
+                bookCount
+                savedBooks {
+                    bookId
+                    title
+                    description
+                    authors
+                    image
+                    link
+                }
+            }
+        }
+    }
+`;
